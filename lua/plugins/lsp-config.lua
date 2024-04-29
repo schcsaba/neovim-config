2fd496fa42d77d463c68b2f6dfac508cbce2a555
Csaba Schnitchen <c.schnitchen@chstudio.fr>
2024-04-29 17:25:19 +0200

    Modify lsp-config settings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,27 +1,41 @@
 return {
 	{
 		"williamboman/mason.nvim",
+    lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
-			})
-		end,
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
 	},
 	{
 		"neovim/nvim-lspconfig",
+    lazy = false,
 		config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
 			lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.intelephense.setup({
         capabilities = capabilities
       })
 
