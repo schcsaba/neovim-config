25a8e8aa46c44ba13ad77a5cf7af1765cf115c77
Csaba Schnitchen <c.schnitchen@chstudio.fr>
2024-04-29 12:24:10 +0200

    Update key maps in lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -21,8 +21,9 @@ return {
 			lspconfig.tsserver.setup({})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
 }
