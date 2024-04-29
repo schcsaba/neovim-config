a976b0b4062aedbe65fb25e7313b006cff1a0c79
Csaba Schnitchen <c.schnitchen@chstudio.fr>
2024-04-29 17:36:52 +0200

    Modify vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.g.background = "light"
+vim.wo.number = true
+vim.wo.relativenumber = true
